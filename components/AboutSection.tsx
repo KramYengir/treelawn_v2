c1a1356b86fc5f8268fb249bd8147e6df6547f0a
KramYengir <kramyengir@gmail.com>
2024-09-25 14:14:34 +0100

    replace images with graphics

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -3,21 +3,32 @@ import React from "react";
 const AboutSection = () => {
   return (
     <section id="about" className="h-[95vh] py-24">
-      <h2>About Us</h2>
-      <div className="rm-container flex flex-wrap items-center justify-evenly gap-12">
+      <h2 className="uppercase text-6xl text-mucky-900 font-bold tracking-wide text-center pb-24 mx-auto">
+        About us
+      </h2>
+      <div className="rm-container flex flex-wrap items-center justify-between gap-12">
         {/* images & logo */}
-        <div className="relative flex-1 grid grid-cols-2 max-w-lg aspect-4/3 overflow-hidden">
-          <div className="overflow-hidden border-4 border-white rounded-xl ">
-            <img src="/images/lawnmower.png" alt="simple lawnmower graphic" />
+        <div className="relative grid grid-cols-2 max-w-lg aspect-4/3 overflow-hidden">
+          <div className="overflow-hidden border-4 border-white bg-gradient-to-br from-apple-800/80 to-apple-950 rounded-2xl ">
+            <img
+              src="/images/lawnmower-opac.png"
+              alt="simple lawnmower graphic"
+            />
           </div>
-          <div className="overflow-hidden border-4 border-white rounded-xl ">
-            <img src="/images/digger.png" alt="simple digger graphic" />
+          <div className="overflow-hidden border-4 border-white bg-gradient-to-bl from-apple-800/80 to-apple-950 rounded-2xl ">
+            <img src="/images/digger-opac.png" alt="simple digger graphic" />
           </div>
-          <div className="overflow-hidden border-4 border-white rounded-xl ">
-            <img src="/images/clippers.png" alt="simple clippers graphic" />
+          <div className="overflow-hidden border-4 border-white bg-gradient-to-tr from-apple-800/80 to-apple-950 rounded-2xl ">
+            <img
+              src="/images/clippers-opac.png"
+              alt="simple clippers graphic"
+            />
           </div>
-          <div className="overflow-hidden border-4 border-white rounded-xl ">
-            <img src="/images/chainsaw.png" alt="simple chainsaw graphic" />
+          <div className="overflow-hidden border-4 border-white bg-gradient-to-tl from-apple-800/80 to-apple-950 rounded-2xl ">
+            <img
+              src="/images/chainsaw-opac.png"
+              alt="simple chainsaw graphic"
+            />
           </div>
           <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] max-w-[40%] rounded-full overflow-hidden">
             <img src="/images/logo.jpg" alt="treelawn logo" />
@@ -26,7 +37,7 @@ const AboutSection = () => {
 
         {/* about us info */}
         <div className="flex-1 flex flex-col items-start gap-4 max-w-lg">
-          <h3 className="uppercase text-3xl font-bold tracking-wide">
+          <h3 className="uppercase text-2xl text-apple-900 font-bold tracking-wide">
             What Treelawn represents
           </h3>
           <p>
