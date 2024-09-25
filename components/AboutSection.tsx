126c22c6cf91f41b840d653ed857118f1a713b03
KramYengir <kramyengir@gmail.com>
2024-09-25 18:27:42 +0100

    update collage

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -6,38 +6,32 @@ const AboutSection = () => {
       <h2 className="uppercase text-6xl text-mucky-900 font-bold tracking-wide text-center pb-24 mx-auto">
         About us
       </h2>
-      <div className="rm-container flex flex-wrap items-center justify-between gap-12">
+      <div className="rm-container flex flex-wrap items-center justify-evenly gap-12">
         {/* images & logo */}
-        <div className="relative grid grid-cols-2 max-w-lg aspect-4/3 overflow-hidden">
-          <div className="overflow-hidden border-4 border-white bg-gradient-to-br from-apple-800/80 to-apple-950 rounded-2xl ">
+        <div className="relative grid grid-cols-2 max-w-md aspect-4/3 overflow-hidden">
+          <div className="overflow-hidden border-4 border-mucky-50 rounded-2xl ">
             <img
-              src="/images/lawnmower-opac.png"
+              src="/images/guy-clipping.jpg"
               alt="simple lawnmower graphic"
             />
           </div>
-          <div className="overflow-hidden border-4 border-white bg-gradient-to-bl from-apple-800/80 to-apple-950 rounded-2xl ">
-            <img src="/images/digger-opac.png" alt="simple digger graphic" />
+          <div className="overflow-hidden border-4 border-mucky-50 rounded-2xl ">
+            <img src="/images/tree-cutting.jpg" alt="simple digger graphic" />
           </div>
-          <div className="overflow-hidden border-4 border-white bg-gradient-to-tr from-apple-800/80 to-apple-950 rounded-2xl ">
-            <img
-              src="/images/clippers-opac.png"
-              alt="simple clippers graphic"
-            />
+          <div className="overflow-hidden border-4 border-mucky-50 rounded-2xl ">
+            <img src="/images/digging.webp" alt="simple clippers graphic" />
           </div>
-          <div className="overflow-hidden border-4 border-white bg-gradient-to-tl from-apple-800/80 to-apple-950 rounded-2xl ">
-            <img
-              src="/images/chainsaw-opac.png"
-              alt="simple chainsaw graphic"
-            />
+          <div className="overflow-hidden border-4 border-mucky-50 rounded-2xl ">
+            <img src="/images/woodcutter.jpg" alt="simple chainsaw graphic" />
           </div>
-          <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] max-w-[40%] rounded-full overflow-hidden">
-            <img src="/images/logo.jpg" alt="treelawn logo" />
+          <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] max-w-[40%] bg-mucky-50 rounded-full overflow-hidden">
+            <img src="/images/logo-v3.png" alt="treelawn logo" />
           </div>
         </div>
 
         {/* about us info */}
         <div className="flex-1 flex flex-col items-start gap-4 max-w-lg">
-          <h3 className="uppercase text-2xl text-apple-900 font-bold tracking-wide">
+          <h3 className="uppercase text-3xl text-apple-900 font-extrabold tracking-wide">
             What Treelawn represents
           </h3>
           <p>
