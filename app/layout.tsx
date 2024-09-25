2cb1bcde8985a845254e62f4bfff67f94b79ff1f
KramYengir <kramyengir@gmail.com>
2024-09-25 14:13:55 +0100

    remove unused fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,7 @@
 import type { Metadata } from "next";
-import { Inter, Poppins, Saira } from "next/font/google";
+import { Saira } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-});
 const saira = Saira({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800", "900"],
