b51bacf0802d3d7175ca7b27c3b22a4a0c04a1a0
KramYengir <kramyengir@gmail.com>
2024-09-26 17:11:41 +0100

    add servicessection

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import AboutSection from "@/components/AboutSection";
+import ServicesSection from "@/components/ServicesSection";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Navbar />
       <Hero />
       <AboutSection />
+      <ServicesSection />
     </div>
   );
 };
