25cb9533f4a10a200542f2f2397ba160684edaa3
KramYengir <kramyengir@gmail.com>
2024-09-28 16:42:12 +0100

    tweak icon position & size & increase h1 size on mobile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,13 +9,18 @@ const Hero = () => {
     >
       <div className="rm-container w-full flex flex-col lg:flex-row items-start lg:items-center justify-evenly gap-x-12 gap-y-12 text-balance">
         <div className="flex-1 flex flex-col items-start justify-center gap-y-4 max-w-md md:max-w-lg lg:max-w-2xl">
-          <h1 className="uppercase text-5xl md:text-7xl xl:text-8xl font-extrabold tracking-wide">
+          {/* <div className="hidden md:flex">
+            <ToolIcons />
+          </div> */}
+          <h1 className="uppercase text-6xl md:text-7xl xl:text-8xl font-extrabold tracking-wide">
             shape your land the <span className="text-apple-600">right</span>{" "}
             way
           </h1>
         </div>
         <div className="flex-1 flex flex-col items-start justify-center gap-x-12 gap-y-6 max-w-lg">
-          <ToolIcons />
+          <div className="flex ">
+            <ToolIcons />
+          </div>
           <p className="font-medium md:text-lg lg:text-xl max-w-md">
             With over two decades of hands-on experience specializing in tree
             work, Treelawn stands as a trusted one-man operation committed to
