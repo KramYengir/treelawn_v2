a2f6f6ac82223569dfa4e42cece1b7a11be15f11
KramYengir <kramyengir@gmail.com>
2024-09-28 16:39:23 +0100

    install shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,46 +1,92 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      aspectRatio: {
-        "4/3": "4 / 3",
-      },
-      colors: {
-        apple: {
-          "50": "#e7ffe5",
-          "100": "#c8ffc7",
-          "200": "#96ff95",
-          "300": "#58fe5c",
-          "400": "#26f52c",
-          "500": "#06dc11",
-          "600": "#00b30c",
-          "700": "#068511",
-          "800": "#0b6913",
-          "900": "#0f5818",
-          "950": "#013208",
-        },
-        mucky: {
-          "50": "#f7f6ef",
-          "100": "#ece8d5",
-          "200": "#dcd2ac",
-          "300": "#c7b67d",
-          "400": "#b69c59",
-          "500": "#a78a4b",
-          "600": "#8f6f3f",
-          "700": "#735535",
-          "800": "#624731",
-          "900": "#423024",
-          "950": "#302018",
-        },
-      },
-    },
+  	extend: {
+  		aspectRatio: {
+  			'4/3': '4 / 3'
+  		},
+  		colors: {
+  			apple: {
+  				'50': '#e7ffe5',
+  				'100': '#c8ffc7',
+  				'200': '#96ff95',
+  				'300': '#58fe5c',
+  				'400': '#26f52c',
+  				'500': '#06dc11',
+  				'600': '#00b30c',
+  				'700': '#068511',
+  				'800': '#0b6913',
+  				'900': '#0f5818',
+  				'950': '#013208'
+  			},
+  			mucky: {
+  				'50': '#f7f6ef',
+  				'100': '#ece8d5',
+  				'200': '#dcd2ac',
+  				'300': '#c7b67d',
+  				'400': '#b69c59',
+  				'500': '#a78a4b',
+  				'600': '#8f6f3f',
+  				'700': '#735535',
+  				'800': '#624731',
+  				'900': '#423024',
+  				'950': '#302018'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
