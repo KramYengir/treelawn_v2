f68ced9131a1a293fd8e9da9b3396dc75b0ead35
KramYengir <kramyengir@gmail.com>
2024-09-28 16:41:12 +0100

    complete basic layout of contact section

---
+++ components/Contact.tsx
@@ -0,0 +1,37 @@
+import { Mail, MapPin, Phone } from "lucide-react";
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section id="contact" className="grid py-24">
+      <div className="rm-container text-mucky-50">
+        <h2 className="h2 text-apple-800">Get In Touch</h2>
+        <div className="grid gap-6 w-full text-lg text-center font-medium tracking-wide">
+          <div className="flex flex-col gap-4 justify-center min-h-48 py-6 rounded-xl bg-[url('../public/images/trees-forest-sm.jpg')] sm:bg-[url('../public/images/trees-forest.jpg')] bg-cover bg-top">
+            <MapPin width={32} height={32} className="mx-auto w-full" />
+            <a
+              href="https://www.google.com/maps?q=Mountrath+Street,+Kilcormac,+Co.+Offaly"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Kilcormac,
+              <br /> Co. Offaly,
+              <br />
+              Ireland
+            </a>
+          </div>
+          <div className="flex flex-col gap-4 justify-center min-h-48 py-6 rounded-xl bg-[url('../public/images/trees-forest-sm.jpg')] sm:bg-[url('../public/images/trees-forest.jpg')] bg-cover bg-center">
+            <Phone width={32} height={32} className="mx-auto w-full" />
+            <a href="tel:+353-1-123-4567">+353 1 123 4567</a>
+          </div>
+          <div className="flex flex-col gap-4 justify-center min-h-48 py-6 rounded-xl bg-[url('../public/images/trees-forest-sm.jpg')] sm:bg-[url('../public/images/trees-forest.jpg')] bg-cover bg-bottom">
+            <Mail width={32} height={32} className="mx-auto w-full" />
+            <a href="mailto:info@treelawn.com">info@treelawn.com</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
