2eaa1d10b010bc0327e70017c0a2889dc8fe94fd
KramYengir <kramyengir@gmail.com>
2024-09-29 12:32:01 +0100

    change to green bg

--- components/ToolIcons.tsx
+++ components/ToolIcons.tsx
@@ -3,16 +3,16 @@ import React from "react";
 const ToolIcons = () => {
   return (
     <div className="flex w-full items-center gap-[4%]">
-      <div className="max-w-16 md:max-w-16 overflow-hidden bg-gradient-to-t from-neutral-950 to-mucky-900 rounded-2xl border-2 border-mucky-50 shadow-lg ">
+      <div className="max-w-16 md:max-w-16 overflow-hidden bg-gradient-to-t from-neutral-950/90 to-apple-900/90 p-1 rounded-2xl border-2 border-mucky-50 shadow-lg ">
         <img src="/images/lawnmower-zoom.png" alt="simple lawnmower graphic" />
       </div>
-      <div className="max-w-16 md:max-w-16 overflow-hidden bg-gradient-to-t from-neutral-950 to-mucky-900 rounded-2xl border-2 border-mucky-50 shadow-lg ">
+      <div className="max-w-16 md:max-w-16 overflow-hidden bg-gradient-to-t from-neutral-950/90 to-apple-900/90 p-1 rounded-2xl border-2 border-mucky-50 shadow-lg ">
         <img src="/images/digger-zoom.png" alt="simple digger graphic" />
       </div>
-      <div className="max-w-16 md:max-w-16 overflow-hidden bg-gradient-to-t from-neutral-950 to-mucky-900 rounded-2xl border-2 border-mucky-50 shadow-lg ">
+      <div className="max-w-16 md:max-w-16 overflow-hidden bg-gradient-to-t from-neutral-950/90 to-apple-900/90 p-1 rounded-2xl border-2 border-mucky-50 shadow-lg ">
         <img src="/images/clippers-zoom.png" alt="simple clippers graphic" />
       </div>
-      <div className="max-w-16 md:max-w-16 overflow-hidden bg-gradient-to-t from-neutral-950 to-mucky-900 rounded-2xl border-2 border-mucky-50 shadow-lg ">
+      <div className="max-w-16 md:max-w-16 overflow-hidden bg-gradient-to-t from-neutral-950/90 to-apple-900/90 p-1 rounded-2xl border-2 border-mucky-50 shadow-lg ">
         <img src="/images/chainsaw-zoom.png" alt="simple chainsaw graphic" />
       </div>
     </div>
