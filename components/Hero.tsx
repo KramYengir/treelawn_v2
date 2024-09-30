096d92c2b6e9f4bcbdbf86e8d4d3fc3e53913b3c
KramYengir <kramyengir@gmail.com>
2024-09-30 16:07:34 +0100

    remove toolicons & add line on small screens

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,20 +5,17 @@ const Hero = () => {
   return (
     <section
       id="hero"
-      className="relative flex flex-col justify-center text-white pt-20 min-h-[90vh] bg-[url('../public/images/garden.webp')] bg-cover bg-center"
+      className="relative flex flex-col justify-center text-mucky-50 pt-20 min-h-[90vh] bg-[url('../public/images/garden.webp')] bg-cover bg-center"
     >
-      <div className="rm-container w-full flex flex-col lg:flex-row items-start lg:items-center justify-evenly gap-x-12 gap-y-12 text-balance">
-        <div className="flex-1 flex flex-col items-start justify-center gap-y-4 max-w-md md:max-w-lg lg:max-w-2xl">
+      <div className="rm-container w-full flex flex-col lg:flex-row items-start lg:items-center justify-evenly gap-x-12  text-balance">
+        <div className="flex-1 flex flex-col items-start justify-center gap-y-4 pb-8 lg:pb-0 max-w-md md:max-w-lg lg:max-w-2xl">
           <h1 className="uppercase text-6xl md:text-7xl xl:text-8xl font-extrabold tracking-wide">
             shape your land the <span className="text-apple-600">right</span>{" "}
             way
           </h1>
         </div>
-        <div className="flex-1 flex flex-col items-start justify-center gap-x-12 gap-y-6 max-w-lg">
-          <div className="flex ">
-            <ToolIcons />
-          </div>
-          <p className="font-medium md:text-lg lg:text-xl max-w-md">
+        <div className="flex-1 flex flex-col items-start justify-center gap-x-12 gap-y-6 pt-8 lg:pt-0 border-t-2 lg:border-0 border-mucky-50/40 max-w-lg">
+          <p className="font-medium tracking-wide md:text-lg lg:text-xl max-w-md">
             With over two decades of hands-on experience specializing in tree
             work, Treelawn stands as a trusted one-man operation committed to
             excellence in outdoor services.
